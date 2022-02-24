47586c1e1c481e122671997c3af92221e5627553
Daniel Viberg <daniel_viberg@outlook.com>
2022-02-24 10:25:04 +0100

    First commit

---
+++ apps.sh
@@ -0,0 +1,14 @@
+#Dotnet sdk
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt-get update -y 
+apt-get install -y apt-transport-https 
+apt-get update  -y
+apt-get install -y dotnet-sdk-6.0
+
+#Github cli
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update -y 
+apt install gh -y 
