fef1ffa8af1851c1f1636c34d8e13b44b9ea32d3
Daniel Viberg <daniel_viberg@outlook.com>
2022-02-24 10:51:54 +0100

    Added docker install

--- apps.sh
+++ apps.sh
@@ -12,3 +12,16 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update -y 
 apt install gh -y 
+
+#docker
+apt-get install \
+    ca-certificates -y \
+    curl -y \
+    gnupg -y \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io
