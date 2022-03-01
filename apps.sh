011cbc541992e66b5a1d167df63a35c4c9941598
Daniel Viberg <daniel_viberg@outlook.com>
2022-03-01 16:38:07 +0100

    Added docker

--- apps.sh
+++ apps.sh
@@ -1,3 +1,10 @@
+#Tools
+apt-get install \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
 #Dotnet sdk
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
@@ -21,3 +28,11 @@ sudo apt-get install keepass2
 #OneDrive
 add-apt-repository ppa:yann1ck/onedrive
 apt install onedrive
+
+#Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
