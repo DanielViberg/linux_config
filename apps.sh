6ca0454559cbc664253b1cee4c9183acf0fe2df3
Daniel Viberg <daniel_viberg@outlook.com>
2022-03-10 15:25:45 +0100

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -44,6 +44,3 @@ apt-get install docker-ce docker-ce-cli containerd.io
 #MySQL
 sudo apt install mysql-server
 sudo service mysql start
-
-#Add fix for caps script in correct dir
-cp FixCapsDelay.sh /etc/profile.d/
